f1c63405391f95a8025b3ee0fe57a3df3d839048
Neel Bhalla <ashdawngary@gmail.com>
2019-04-11 08:13:22 -0400

    added world file

---
+++ src/conveyorio/World.java
@@ -0,0 +1,17 @@
+package conveyorio;
+
+import structures.Structure;
+
+/*
+ * Rendering Chunk, Can Render noGraphic or Graphics.
+ */
+public class World {
+	
+	public World() {
+		
+	}
+	public Structure getTileAt(int x, int y) {
+		return null;
+	}
+}
+
