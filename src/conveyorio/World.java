5c16535d3e56acbf51f845e605cc8573495e979d
Michael Alperovich <misha.alperovich1@gmail.com>
2019-04-22 09:54:09 -0400

    Hash function for point class

--- src/conveyorio/World.java
+++ src/conveyorio/World.java
@@ -24,14 +24,24 @@ public class World {
 
 
 class Point {
-	public int x;
-	public int y;
+	private int x;
+	private int y;
+
 	public Point(int x,int y) {
-		
+		this.x = x;
+		this.y = y;
 	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
 	@Override
 	public int hashCode() {
-		
-		return 1;
+		return (x * 31 + y) % (int)(10E9 + 7);
 	}
 }
