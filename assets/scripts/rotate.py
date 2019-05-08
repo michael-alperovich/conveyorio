b0379e499c40ec17616b34d575be45ea4401b4bb
Neel Bhalla <ashdawngary@gmail.com>
2019-05-08 13:25:00 -0400

    fixed transparency

--- assets/scripts/rotate.py
+++ assets/scripts/rotate.py
@@ -6,5 +6,17 @@ def createRotator(prefix):
     for ang in range(0,360):
         new_image = im.rotate(ang,expand = False,center = center)
         new_image.save(prefix+"_rotateframe(%s).png"%(ang))
-        
-        
+        fixBackground(prefix+"_rotateframe(%s).png"%(ang))
+def fixBackground(im):
+    img = Image.open(im)
+    img = img.convert("RGBA")
+
+    pixdata = img.load()
+
+    width, height = img.size
+    for y in xrange(height):
+        for x in xrange(width):
+            if pixdata[x, y] == (255, 255, 255, 255):
+                pixdata[x, y] = (255, 255, 255, 0)
+
+    img.save(im, "PNG")
