61034d585745998b2b7ccaf0169689ddd3f097f8
Neel Bhalla <ashdawngary@gmail.com>
2019-06-05 17:57:39 -0400

    added threading for loading assets 9s to 1s load time! and new rev 3 release

--- src/conveyorio/Main.java
+++ src/conveyorio/Main.java
@@ -24,58 +24,117 @@ public class Main {
 
 	
 	public static Responses loadGraphics() {
-		
+		Thread t1 = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				long cTime = System.currentTimeMillis();
+				try {
+					ConveyorAssets.loadAssets();
+				} catch (Exception e) {
+					System.out.println("[-] Conveyor Assets Failed to Load Assets");
+					e.printStackTrace();	
+				}
+				System.out.println("ConveyorAssets Time: "+(System.currentTimeMillis()-cTime));
+			}});
+		Thread t2 = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				long cTime = System.currentTimeMillis();
+				try {
+
+					ItemsAssets.loadAssets();
+				} catch (IOException e) {
+					System.out.println("[-] Coal Assets Failed to Load Assets");
+					e.printStackTrace();
+
+				}
+
+				System.out.println("CoalAssets Time: "+(System.currentTimeMillis()-cTime));
+			}});
+		Thread lightThread = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				long cTime = System.currentTimeMillis();
+				try {
+					ChestAssets.loadAssets();
+				} catch (Exception e) {
+					System.out.println("[-] Chest Assets Failed to Load Assets");
+					e.printStackTrace();
+				}
+
+				System.out.println("Chest Time: "+(System.currentTimeMillis()-cTime));
+				cTime = System.currentTimeMillis();
+				try {
+					MiscAssets.loadAssets();
+				} catch (Exception e) {
+					System.out.println("[-] Misc Assets Failed to Load Assets");
+					e.printStackTrace();
+
+				}
+
+				System.out.println("MiscAssets Time: "+(System.currentTimeMillis()-cTime));
+				cTime = System.currentTimeMillis();
+				try {
+					FurnaceAssets.loadAssets();
+				} catch (Exception e) {
+					System.out.println("[-] Furnace Assets Failed to Load Assets");
+					e.printStackTrace();
+
+				}
+
+				System.out.println("FurnaceAssets Time: "+(System.currentTimeMillis()-cTime));
+			}});
+		Thread singleInserter = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				long cTime = System.currentTimeMillis();
+				try {
+					SingleInserterAssets.loadAssets();
+				} catch (Exception e) {
+					System.out.println("[-] Single Inserter Assets Failed to Load Assets");
+					e.printStackTrace();
+
+				}
+
+				System.out.println("SingleInserter Time: "+(System.currentTimeMillis()-cTime));
+					
+			}});	
+		Thread curvedLoad = new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				long cTime = System.currentTimeMillis();
+				try {
+					CurvedConveyorAssets.loadAssets();
+				} catch (Exception e) {
+					System.out.println("[-] Curved Conveyor Assets Failed to Load Assets");
+					e.printStackTrace();
+				}
+
+				System.out.println("CurvedAssets Time: "+(System.currentTimeMillis()-cTime));
+				cTime = System.currentTimeMillis();
+				
+			}});	
+				
 		System.out.println("<-----------------Loading Assets---------------->");
-		try {
-			ConveyorAssets.loadAssets();
-		} catch (Exception e) {
-			System.out.println("[-] Conveyor Assets Failed to Load Assets");
-			e.printStackTrace();
-			
-		}
-		try {
-
-			ItemsAssets.loadAssets();
-		} catch (IOException e) {
-			System.out.println("[-] Coal Assets Failed to Load Assets");
-			e.printStackTrace();
-
-		}
-		try {
-			SingleInserterAssets.loadAssets();
-		} catch (Exception e) {
-			System.out.println("[-] Single Inserter Assets Failed to Load Assets");
-			e.printStackTrace();
-
-		}
-		try {
-			ChestAssets.loadAssets();
-		} catch (Exception e) {
-			System.out.println("[-] Chest Assets Failed to Load Assets");
-			e.printStackTrace();
-		}
-		try {
-			MiscAssets.loadAssets();
-		} catch (Exception e) {
-			System.out.println("[-] Misc Assets Failed to Load Assets");
-			e.printStackTrace();
-
-		}
-		try {
-			FurnaceAssets.loadAssets();
-		} catch (Exception e) {
-			System.out.println("[-] Furnace Assets Failed to Load Assets");
-			e.printStackTrace();
-
-		}
+		long start = System.currentTimeMillis();
+		t1.start();
+		t2.start();
+		lightThread.start();
+		singleInserter.start();
+		curvedLoad.start();
 		
-		try {
-			CurvedConveyorAssets.loadAssets();
-		} catch (Exception e) {
-			System.out.println("[-] Curved Conveyor Assets Failed to Load Assets");
-			e.printStackTrace();
-		}
-		
+		while(t1.isAlive() || t2.isAlive() || lightThread.isAlive());
+		while(singleInserter.isAlive() || curvedLoad.isAlive());
+		long diff = System.currentTimeMillis() - start;
+		System.out.println("Overall Load time: "+diff+" ms");
 		return Responses.OK;
 	}
 }
